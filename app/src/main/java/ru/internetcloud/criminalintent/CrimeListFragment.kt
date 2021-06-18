7498f81a288a631ed133c39533aa3afd3b57d5f4
harisov-mobile <v8spec@mail.ru>
2021-06-18 08:20:56 +0300

    создал CrimeListRecyclerView

---
+++ app/src/main/java/ru/internetcloud/criminalintent/CrimeListFragment.kt
@@ -0,0 +1,93 @@
+package ru.internetcloud.criminalintent
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.internetcloud.criminalintent.model.Crime
+import ru.internetcloud.criminalintent.model.CrimeListViewModel
+
+private const val TAG = "rustam"
+
+class CrimeListFragment: Fragment() {
+
+    // свойства класса: (с отложенной инициализацией)
+    private val crimeListViewModel: CrimeListViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeListViewModel::class.java)
+    }
+
+    private lateinit var crimeListRecyclerView: RecyclerView
+    private var crimeListAdapter: CrimeAdapter? = null
+
+    // статика
+    companion object {
+        fun newInstance(): CrimeListFragment {
+            return CrimeListFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.i(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_crime_list, container, false)
+        crimeListRecyclerView = view.findViewById(R.id.crime_list_recycler_view) as RecyclerView
+        crimeListRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        updateUI()
+
+        return view
+    }
+
+    // внутренний класс CrimeHolder
+    private inner class CrimeHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        private lateinit var crime: Crime
+
+        val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+
+        fun bind(crime: Crime) {
+            this.crime = crime
+            titleTextView.text = crime.title
+            dateTextView.text = crime.date.toString()
+        }
+
+    }
+
+    // внутренний класс адаптер
+    private inner class CrimeAdapter(var crimes: List<Crime>): RecyclerView.Adapter<CrimeHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+            val itemView = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(itemView)
+        }
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.bind(crime)
+        }
+
+        override fun getItemCount(): Int {
+            return crimes.size
+        }
+
+    }
+
+    private fun updateUI() {
+        val crimes = crimeListViewModel.crimes
+        crimeListAdapter = CrimeAdapter(crimes)
+        crimeListRecyclerView.adapter = crimeListAdapter
+    }
+}

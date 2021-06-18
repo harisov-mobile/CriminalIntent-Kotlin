10946fab9380e850362621a2aaecee1122c6d77a
harisov-mobile <v8spec@mail.ru>
2021-06-18 06:31:34 +0300

    добавлен фрагмент CrimeFragment, добавлен класс CrimeListViewModel

---
+++ app/src/main/java/ru/internetcloud/criminalintent/CrimeFragment.kt
@@ -0,0 +1,93 @@
+package ru.internetcloud.criminalintent
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import ru.internetcloud.criminalintent.model.Crime
+
+private const val TAG = "rustam"
+
+class CrimeFragment: Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.fragment_crime, container, false)
+
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
+
+//        Log.i(TAG, "Процедура onCreateView, содержимое EditText еще не восстановлено к этому моменту после" +
+//                "поворота titleField.text = " + titleField.text)
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // TextWatcher нужно навешивать здесь, а не в onCreate или onCreateView, т.к. там еще не восстановлено
+        // EditText и слушатели будут "дергаться" лишний раз
+
+        // Log.i(TAG, "Процедура onStart, содержимое EditText уже восстановлено к этому моменту после поворота," +
+        //            " оно заполнено: titleField.text = " + titleField.text)
+
+        val titleWatcher = object : TextWatcher {
+
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            // Это пространство оставлено пустым специально
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                crime.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+            // И это
+            }
+        }
+
+        titleField.addTextChangedListener(titleWatcher)
+
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener { _, isChecked -> crime.isSolved = isChecked }
+        }
+
+    }
+
+
+}

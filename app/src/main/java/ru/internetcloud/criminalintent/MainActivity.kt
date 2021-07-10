791f88a0c27b53874fea68b6a99142a4ba6ef32f
harisov-mobile <v8spec@mail.ru>
2021-07-10 08:48:35 +0300

    Добавил логи.

--- app/src/main/java/ru/internetcloud/criminalintent/MainActivity.kt
+++ app/src/main/java/ru/internetcloud/criminalintent/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
         val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
 
         if (currentFragment == null) {
+            Log.i(TAG, "currentFragment == null")
             val fragment = CrimeListFragment.newInstance()
             supportFragmentManager.beginTransaction()
                 .add(R.id.fragment_container, fragment)
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
     }
 
     override fun onCrimeSelected(crimeId: UUID) {
-        // Log.i(TAG, "MainActivity.onCrimeSelected: $crimeId")
+         Log.i(TAG, "MainActivity.onCrimeSelected: $crimeId")
         val fragment = CrimeFragment.newInstance(crimeId)
 
         supportFragmentManager.beginTransaction()
